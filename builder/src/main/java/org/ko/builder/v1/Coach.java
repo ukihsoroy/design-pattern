983a6bfba3b6611ac8ba3bf9f5cd8421031bf55e
uzies <ko.shen@hotmail.com>
2019-05-29 23:16:14 +0800

    #建造者模式

---
+++ builder/src/main/java/org/ko/builder/v1/Coach.java
@@ -0,0 +1,23 @@
+package org.ko.builder.v1;
+
+public class Coach {
+
+    private CourseBuilder courseBuilder;
+
+    public void setCourseBuilder(CourseBuilder courseBuilder) {
+        this.courseBuilder = courseBuilder;
+    }
+
+    public Course makeCourse(String courseName,
+                             String coursePPT,
+                             String courseVideo,
+                             String courseArticle,
+                             String courseQA) {
+        this.courseBuilder.buildCourseName(courseName);
+        this.courseBuilder.buildCoursePPT(coursePPT);
+        this.courseBuilder.buildCourseArticle(courseArticle);
+        this.courseBuilder.buildCourseVideo(courseVideo);
+        this.courseBuilder.buildCourseQA(courseQA);
+        return this.courseBuilder.makeCourse();
+    }
+}
---
+++ builder/src/main/java/org/ko/builder/v2/Test.java
@@ -0,0 +1,21 @@
+package org.ko.builder.v2;
+
+public class Test {
+    public static void main(String[] args) {
+        Course course1 = new Course.CourseBuilder()
+                .buildCourseName("Java课程精讲")
+                .buildCourseArticle("Java课程精讲手机")
+                .buildCoursePPT("Java课程精讲PPT")
+                .buildCourseVideo("Java课程精讲视频")
+                .build();
+
+        System.out.println(course1.toString());
+
+        Course course2 = new Course.CourseBuilder()
+                .buildCourseName("Java课程精讲")
+                .buildCourseVideo("Java课程精讲视频")
+                .build();
+
+        System.out.println(course2.toString());
+    }
+}
